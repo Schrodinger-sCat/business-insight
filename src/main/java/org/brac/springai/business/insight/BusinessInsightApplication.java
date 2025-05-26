f6035a5723bbbfd43f1231d10dbd98ccd74a3e59
Md. Toyeb <md.toyeb@bracits.com>
2025-05-26 11:31:54 +0600

    initial commit

---
+++ src/main/java/org/brac/springai/business/insight/BusinessInsightApplication.java
@@ -0,0 +1,13 @@
+package org.brac.springai.business.insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessInsightApplication.class, args);
+    }
+
+}
